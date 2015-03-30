032fcd6d1a57b27ea447396296515d1d8176980f
rrajath <rrajath16@yahoo.com>
2015-03-29 19:05:10 -0700

    Add sliding tabs

--- app/src/main/java/com/rrajath/orange/MainActivity.java
+++ app/src/main/java/com/rrajath/orange/MainActivity.java
@@ -2,31 +2,53 @@ package com.rrajath.orange;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    private Toolbar toolbar;
+    private Toolbar mToolbar;
+    private SlidingTabLayout mSlidingTabs;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set main content view
-        setContentView(R.layout.activity_main_appbar);
+        setContentView(R.layout.activity_main);
 
-        // Set toolbar
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
+        // Set Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         // Set fragment for navigation drawer
         NavDrawerFragment drawerFragment = (NavDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.nav_fragment);
-        drawerFragment.setup((DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        drawerFragment.setup((DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+
+        mViewPager = (ViewPager)findViewById(R.id.tab_pager);
+        mViewPager.setAdapter(new SlidingTabsPagerAdapter(getSupportFragmentManager()));
+        mSlidingTabs = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.abc_primary_text_material_dark);
+            }
+        });
+        mSlidingTabs.setViewPager(mViewPager);
     }
 
     @Override
@@ -38,9 +60,6 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -60,5 +79,50 @@ public class MainActivity extends ActionBarActivity {
         }
         
         return super.onOptionsItemSelected(item);
+    }
+
+    class SlidingTabsPagerAdapter extends FragmentPagerAdapter {
+
+        String[] hnCategories;
+        public SlidingTabsPagerAdapter(FragmentManager fm) {
+            super(fm);
+            hnCategories = getResources().getStringArray(R.array.sliding_tabs_array);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return hnCategories[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TabFragment.getInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return hnCategories.length;
+        }
+    }
+
+    public static class TabFragment extends Fragment {
+        public static TabFragment getInstance(int position) {
+            TabFragment fragment = new TabFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.tab_fragment, container, false);
+            TextView textView = (TextView) view.findViewById(R.id.tab_fragment_title);
+            Bundle args = getArguments();
+            if (args != null) {
+                textView.setText("Fragment number: " + args.getInt("position"));
+            }
+            return view;
+        }
     }
 }
--- app/src/main/java/com/rrajath/orange/NavDrawerFragment.java
+++ app/src/main/java/com/rrajath/orange/NavDrawerFragment.java
@@ -1,5 +1,6 @@
 package com.rrajath.orange;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,9 +9,12 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.rrajath.orange.data.NavDrawerItem;
 
@@ -24,8 +28,9 @@ public class NavDrawerFragment extends Fragment {
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-    private RecyclerView recyclerView;
-    private NavDrawerAdapter navDrawerAdapter;
+    private RecyclerView mRecyclerView;
+    private ClickListener mClickListener;
+    private NavDrawerAdapter mNavDrawerAdapter;
     private List<NavDrawerItem> mNavDrawerItems;
 
     public void setup(DrawerLayout drawerLayout, final Toolbar toolbar) {
@@ -63,19 +68,56 @@ public class NavDrawerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.nav_drawer_fragment, container, false);
-        recyclerView = (RecyclerView) view.findViewById(R.id.drawerList);
-        navDrawerAdapter = new NavDrawerAdapter(getActivity(), buildNavDrawerItems());
-        recyclerView.setAdapter(navDrawerAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.drawerList);
+        mNavDrawerAdapter = new NavDrawerAdapter(getActivity(), buildNavDrawerItems());
+        mRecyclerView.setAdapter(mNavDrawerAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), mRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(), "onClick", Toast.LENGTH_SHORT).show();
+            }
+        }));
         return view;
     }
 
     private List<NavDrawerItem> buildNavDrawerItems() {
         mNavDrawerItems = new ArrayList<>();
-        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_class_black, "Favorites"));
-        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_settings_applications_black, "Settings"));
-        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_grade_black, "Rate the app"));
-        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_comment_black, "Feedback"));
+        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_class_grey600_36dp, "Favorites"));
+        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_settings_applications_grey600_36dp, "Settings"));
+        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_grade_grey600_36dp, "Rate the app"));
+        mNavDrawerItems.add(new NavDrawerItem(R.drawable.ic_comment_grey600_36dp, "Feedback"));
         return mNavDrawerItems;
+    }
+
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+        GestureDetector gestureDetector;
+        public RecyclerTouchListener(Context context, RecyclerView recyclerView, ClickListener clickListener) {
+            mClickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && mClickListener != null && gestureDetector.onTouchEvent(e)) {
+                mClickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+    }
+
+    public static interface ClickListener {
+        public void onClick(View view, int position);
     }
 }
