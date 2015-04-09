9c2cbe4680d225b030833b1a5fc3005e7ae6be34
rrajath <rrajath16@yahoo.com>
2015-04-08 22:44:29 -0700

    Use fade in animation instead of slide up

--- app/src/main/java/com/rrajath/orange/utils/AnimationUtils.java
+++ app/src/main/java/com/rrajath/orange/utils/AnimationUtils.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.animation.Animation;
 
-import com.rrajath.orange.R;
-
 /**
  * Created by rrajath on 4/8/15.
  */
 public class AnimationUtils {
 
     public static void decelerate(Context context, RecyclerView.ViewHolder viewHolder) {
-        Animation animation = android.view.animation.AnimationUtils.loadAnimation(context, R.anim.decelerate);
+        Animation animation = android.view.animation.AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
+        animation.setDuration(1000);
         viewHolder.itemView.startAnimation(animation);
     }
 }
